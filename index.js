4142c714f5e624deea3d1dd00bf1728cb3a84ed2
Pete VS <pete_vs88@me.com>
2020-09-04 15:25:21 -0400

    finished task 6

--- index.js
+++ index.js
@@ -86,15 +86,19 @@ function getWinnersByYear(data, getWinners, getYears) {
   return winnersByYear;
 }
 
+console.log("Winners By Year:");
 console.log(getWinnersByYear(fifaData, getWinners, getYears));
 
 /* Task 6: Write a function called `getAverageGoals` that accepts a parameter `data` and returns the the average number of home team goals and away team goals scored per match (Hint: use .reduce and do this in 2 steps) */
 
-function getAverageGoals(/* code here */) {
-  /* code here */
+function getAverageGoals(data) {
+  let totalHomeGoals = data.reduce(function (accumulator, item) {
+    return accumulator + item["Home Team Goals"];
+  }, 0);
+  return Math.round(totalHomeGoals / data.length);
 }
 
-getAverageGoals();
+console.log(getAverageGoals(fifaData));
 
 /// STRETCH 🥅 //
 
