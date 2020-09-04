77843fcbd385d9f8d825bd92f34b7c9d4ad3d4e7
Pete VS <pete_vs88@me.com>
2020-09-03 21:41:43 -0400

    Finished tasks 1-4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import { fifaData } from './fifa.js';
+import { fifaData } from "./fifa.js";
 console.log(fifaData);
 
-console.log('its working');
+// console.log("its working");
 // ⚽️ M  V P ⚽️ //
 
 /* Task 1: Investigate the data above. Practice accessing data by console.log-ing the following pieces of data 
@@ -12,32 +12,52 @@ console.log('its working');
 (d) Away Team goals for 2014 world cup final
 (e) Winner of 2014 world cup final */
 
+console.log("2014 World Cup Final");
+//Filter Out 2014 World Cup Final
+let wcf2014 = fifaData.filter(
+  (games) => games.Year === 2014 && games.Stage === "Final"
+);
+
+//Get Home Team Name
+let wcfHomeTeam = wcf2014.map(function (item) {
+  return item["Home Team Name"];
+});
+console.log(wcfHomeTeam);
+
+//Get Away Team Name
+let wcfAwayTeam = wcf2014.map((item) => item["Away Team Name"]);
+console.log(wcfAwayTeam);
+
+//Get Home Team Goals
+let wcfHomeTeamGoals = wcf2014.map((item) => item["Home Team Goals"]);
+console.log(wcfHomeTeamGoals);
+
+//Get Away Team Goals
+let wcfAwayTeamGoals = wcf2014.map((item) => item["Away Team Goals"]);
+console.log(wcfAwayTeamGoals);
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
-
-    /* code here */
-
-};
+function getFinals(data) {
+  return data.filter((games) => games.Stage === "Final");
+}
+console.log("All The FIFA Finals");
+console.log(getFinals(fifaData));
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
-function getYears(/* code here */) {
+function getYears(data, getFinals) {
+  /* code here */
+  return getFinals(data).map((item) => item.Year);
+}
+console.log("List of All The Finals Years");
+console.log(getYears(fifaData, getFinals));
 
-    /* code here */
-
-};
-
-getYears();
-
-/* Task 4: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */ 
+/* Task 4: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */
 
 function getWinners(/* code here */) {
-
-    /* code here */
-
-};
+  /* code here */
+}
 
 getWinners();
 
@@ -48,19 +68,15 @@ Parameters:
  * callback function getYears
  */
 
-function getWinnersByYear(/* code here */) {
-
-};
+function getWinnersByYear(/* code here */) {}
 
 getWinnersByYear();
 
 /* Task 6: Write a function called `getAverageGoals` that accepts a parameter `data` and returns the the average number of home team goals and away team goals scored per match (Hint: use .reduce and do this in 2 steps) */
 
 function getAverageGoals(/* code here */) {
-
-    /* code here */
-
-};
+  /* code here */
+}
 
 getAverageGoals();
 
@@ -72,32 +88,24 @@ Hint: Investigate your data to find "team initials"!
 Hint: use `.reduce` */
 
 function getCountryWins(/* code here */) {
-
-    /* code here */
-
-};
+  /* code here */
+}
 
 getCountryWins();
-
 
 /* Stretch 3: Write a function called getGoals() that accepts a parameter `data` and returns the team with the most goals score per appearance (average goals for) in the World Cup finals */
 
 function getGoals(/* code here */) {
-
-    /* code here */
-
-};
+  /* code here */
+}
 
 getGoals();
-
 
 /* Stretch 4: Write a function called badDefense() that accepts a parameter `data` and calculates the team with the most goals scored against them per appearance (average goals against) in the World Cup finals */
 
 function badDefense(/* code here */) {
-
-    /* code here */
-
-};
+  /* code here */
+}
 
 badDefense();
 
